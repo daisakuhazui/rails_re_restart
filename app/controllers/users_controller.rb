73223e5764df45f55a94f386d64a4ef73d6285bb
Daisaku Hazui <h.daisaku.biwabiwako@gmail.com>
2017-10-01 14:02:20 +0000

    Finish user index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  before_action :authenticate_user!, :except=>[:show]
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
+  
 end
